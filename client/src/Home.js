6f614200fef8f73b163e946f766086ecce355615
ravimaddi <ravimaddi18@gmail.com>
2020-04-02 00:05:38 +0530

    added express

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,17 +3,18 @@ import React, { Component } from "react";
 
 class Home extends Component {
  
-      requestProfile = () => {
+  componentDidMount=()=>{
+      
         var oauthUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=81gad30pe28yok&redirect_uri=https://master.dfhwi69jafa7m.amplifyapp.com/auth&state=0.06018782529263844&scope=r_liteprofile%20r_emailaddress%20w_member_social`
         
           window.location.href=`${oauthUrl}`
         
-      };
+    }
 
       render(){
           return(
               <div style={{align:'center'}}>
-            <button  onClick={this.requestProfile}>LinkedIn Login</button>
+            <button  >LinkedIn Login</button>
             
             </div>
           )
