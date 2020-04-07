69657043061c5487d19e07bfdb5a9652bb13fe84
jimin <jimin@pop-os.localdomain>
2020-04-07 13:22:00 +0530

    changes to twitter integration

--- src/Twitter.js
+++ src/Twitter.js
@@ -2,17 +2,6 @@ import React,{useState,useEffect} from "react";
 import axios from "axios";
 import photo from "./twitterLogin.png";
 
-// class Twitter extends React.Component {
-//   render() {
-//     return (
-//       <div style={{ textAlign: "center" }}>
-//         <button onClick={handleTwitter}>Twitter Login</button>
-//       </div>
-//     );
-//   }
-// }
-const API_KEY=''
-const API_SECRET_KEY=''
 export default function Twitter() {
   const handleClick=()=>{
     window.location.href ='https://3xf4trcnyk.execute-api.us-east-1.amazonaws.com/dev/twitter/login'
